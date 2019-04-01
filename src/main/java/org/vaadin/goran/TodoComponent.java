1928cba557a0917d5d135cbd8518ab7df56664dd
Goran <gatanaso@users.noreply.github.com>
2019-04-01 16:14:13 +0300

    Step 3.3 added done button

--- src/main/java/org/vaadin/goran/TodoComponent.java
+++ src/main/java/org/vaadin/goran/TodoComponent.java
@@ -4,7 +4,10 @@ import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.HasStyle;
 import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.icon.VaadinIcon;
 
 import java.util.function.Consumer;
 
@@ -24,6 +27,20 @@ public class TodoComponent extends Component implements HasStyle, HasComponents 
         add(description);
 
         // done button
+        Button doneBtn = new Button();
+
+        doneBtn.addClassName("todo-action");
+
+        doneBtn.setIcon(new Icon(VaadinIcon.CHECK));
+
+        doneBtn.addClickListener(event -> {
+            todo.setDone(true);
+            doneBtn.setEnabled(false);
+            addClassName("done");
+
+        });
+
+        add(doneBtn);
 
         // delete button
     }
